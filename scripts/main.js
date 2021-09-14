0e5e09a2d7d3fddc83f53a05c53f2f999bf462d7
betty <betty.nabuuso@moringaschool.com>
2021-09-14 11:40:54 +0300

    changes in the files

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,6 @@
 $(document).ready(function(){
-    $('#design').hide();
-});
+    $("#design-image").click(function(){
+      $("#design-image").slideDown('1500').hide('1000');
+      $("#design").show('1500');
+    });
+  });
