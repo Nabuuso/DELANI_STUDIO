b3ed5c66583a862c40f7c0e5483725c464430940
betty <betty.nabuuso@moringaschool.com>
2021-09-14 13:03:22 +0300

    changes in the files

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,27 @@
+// $(document).ready(function(){
+//     $("#design-image").click(function(){
+//       $("#design-image").slideDown('1500').hide('1000');
+//       $("#design").show('1500');
+//     });
+//   });
 $(document).ready(function(){
-    $("#design-image").click(function(){
-      $("#design-image").slideDown('1500').hide('1000');
-      $("#design").show('1500');
+    $("#design").hide();
+    $("#development").hide();
+    $("#product").hide();
+    $(".imgtoggle-1, .imgtoggle-1").click(function(){
+        $("#design").toggle();
+        $(".imgtoggle-1").toggle();
     });
-  });
+
+    $(".imgtoggle-2, .imgtoggle-2").click(function(){
+        $("#development").toggle();
+        $(".imgtoggle-2").toggle();
+    });
+
+    $(".imgtoggle-3, .imgtoggle-3").click(function(){
+        $("#product").toggle();
+        $(".imgtoggle-3").toggle();
+    });
+});
+
+
